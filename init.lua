df2ef0e899cc5e27af614823c457a23540842680
Nathan Donze <ndonze@gmail.com>
2024-09-02 12:05:53 -0500

    Set colorcolumn to 120

--- init.lua
+++ init.lua
@@ -157,6 +157,8 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.colorcolumn = '120'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
